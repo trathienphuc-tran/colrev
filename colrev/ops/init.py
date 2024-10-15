e07c3ed238228ca8598a01bd698354d93175f118
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2024-10-15 08:04:01 +0200

    install package dependencies (fix)
    Signed-off-by: Gerit Wagner <gerit.wagner@uni-bamberg.de>

--- colrev/ops/init.py
+++ colrev/ops/init.py
@@ -384,10 +384,10 @@ class Initializer:
         settings = self.review_manager.settings
 
         # Note : to avoid file setup at colrev status (calls data_operation.main)
-        # data_operation = self.review_manager.get_data_operation(
-        #     notify_state_transition_operation=False
-        # )
-        # data_operation.main(silent_mode=True)
+        data_operation = self.review_manager.get_data_operation(
+            notify_state_transition_operation=False
+        )
+        data_operation.main(silent_mode=True)
         self.review_manager.logger.info("Set up %s", self.review_type)
 
         for source in settings.sources:
