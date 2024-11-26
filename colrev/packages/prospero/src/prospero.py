5d26035d4efa7186d5ceb7f235e25fd9882f9625
Phuc Tran <tra-thien-phuc.tran@stud.uni-bamberg.de>
2024-11-26 11:57:13 +0000

    define heuristic class method, first draft

--- colrev/packages/prospero/src/prospero.py
+++ colrev/packages/prospero/src/prospero.py
@@ -7,6 +7,7 @@ import colrev.package_manager.package_settings
 from colrev.constants import SearchType
 from colrev.constants import SearchSourceHeuristicStatus
 from pydantic import Field
+from pathlib import Path
 
 @zope.interface.implementer(colrev.package_manager.interfaces.SearchSourceInterface)
 class ProsperoSearchSource:
@@ -24,3 +25,16 @@ class ProsperoSearchSource:
     #heuristic status likely supported, how to confirm?
 
     db_url = "https://www.crd.york.ac.uk/prospero/"
+
+    @classmethod
+    def heurtistic(cls, filename: Path, data: str)-> float: #return dict or float? 
+        
+        confidence-level = 0.1
+
+        if data.count("Prospero") > 1: #case sensitive? if able to count page number: data.count("Prospero") == data.pagenumber + 3
+            confidence_level = 1.0
+        elif data.find("Date of registration in PROSPERO"):
+            confidence_level = 1.0
+        
+        return confidence_level
+        
