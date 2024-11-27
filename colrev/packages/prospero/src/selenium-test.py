41721c0456bc32fb4db68a9b48491c3d11e20ee5
Phuc Tran <tra-thien-phuc.tran@stud.uni-bamberg.de>
2024-11-27 12:27:57 +0000

    adjusted selenium-test.py for prospero

--- colrev/packages/prospero/src/selenium-test.py
+++ colrev/packages/prospero/src/selenium-test.py
@@ -10,11 +10,12 @@ chrome_options.add_argument('--headless')
 
 service = Service(executable_path=r'/home/vscode/bin/chromedriver-linux64/chromedriver')
 driver = webdriver.Chrome(options = chrome_options, service=service)
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pypi")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+driver.get("https://www.crd.york.ac.uk/prospero/")
+print(driver.title)
+assert "PROSPERO" in driver.title
+#elem = driver.find_element(By.NAME, "q")
+#elem.clear()
+#elem.send_keys("pypi")
+#elem.send_keys(Keys.RETURN)
+#assert "No results found." not in driver.page_source
 #driver.close()
